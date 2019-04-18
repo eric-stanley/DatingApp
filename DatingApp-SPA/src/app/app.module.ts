020f15b8e3e904facb22ac906e3467780baa3c8a
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-04-18 16:57:33 +0530

    Added User Interface

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +14,20 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { environment } from 'src/environments/environment';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -26,20 +37,34 @@ import { AuthGuard } from './_guards/auth.guard';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      NgxGalleryModule,
+      TabsModule.forRoot(),
+      JwtModule.forRoot({
+        config: {
+          tokenGetter,
+          whitelistedDomains: [environment.appDomain],
+          blacklistedRoutes: [environment.appDomain + '/api/auth']
+        }
+      })
    ],
    providers: [
      AuthService,
      ErrorInterceptorProvider,
      AlertifyService,
-     AuthGuard
+     AuthGuard,
+     UserService,
+     MemberDetailResolver,
+     MemberListResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/environments/environment.ts
+++ DatingApp-SPA/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appDomain: 'localhost:5000',
+  appUrl: 'http://localhost:5000/',
+  apiUrl: 'http://localhost:5000/api/'
 };
 
 /*

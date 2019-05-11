95b2fc1832c15e6fdbff33b73a0654fa7729fab2
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-05-12 00:40:25 +0530

    Changes in home component

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  isLoggedIn = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.authService.loggedIn();
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.authService.logout();
   }
 
   registerToggle() {
